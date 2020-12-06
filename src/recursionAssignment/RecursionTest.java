3e70512a1a0db95e9317ea19e28ba717a8755c0c
Jeffrey Lin <jeffrey.lin21@ycdsbk12.ca>
2020-12-06 17:25:16 +0000

    Described test cases for question 1 and 2

--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -4,102 +4,124 @@ import org.junit.jupiter.api.Test;
 
 import recursionAssignment.Recursion;
 
+/**
+* ICS4U TDD and Recursion Assignment Test Cases
+* @author: J. Lin
+*
+*/
+
 public class RecursionTest{
     
-    @Test
-    public void Test1_1(){
-      assertEquals(0, Recursion.count7(0));
-    }
+  // Test cases for count7
 
-    @Test
-    public void Test1_2(){
-      assertEquals(1, Recursion.count7(7));
-    }
+  // Test when there are no 7's in the int, expects 0
+  @Test
+  public void Test1_1() {
+    assertEquals(0, Recursion.count7(0));
+  }
 
-    @Test
-    public void Test1_3(){
-      assertEquals(3, Recursion.count7(777));
-    }
+  // Test when there is one 7 in the int, expects 1
+  @Test
+  public void Test1_2() {
+    assertEquals(1, Recursion.count7(7));
+  }
 
-    @Test
-    public void Test1_4(){
-      assertEquals(3, Recursion.count7(777638492));
-    }
+  // Test when there are only 7's in the int, expects 3
+  @Test
+  public void Test1_3() {
+    assertEquals(3, Recursion.count7(777));
+  }
 
-    @Test
-    public void Test1_5(){
-      assertEquals(2, Recursion.count7(12345677));
-    }
+  // Test when there are 7's and other digits in the int, expects 3
+  @Test
+  public void Test1_4() {
+    assertEquals(3, Recursion.count7(777638492));
+  }
 
-    @Test
-    public void Test1_6(){
-      assertEquals(3, Recursion.count7(7187371));
-    }
+  // Test when there are 7's only at the end and other digits in the int, expects 2
+  @Test
+  public void Test1_5() {
+    assertEquals(2, Recursion.count7(12345677));
+  }
 
-    @Test
-    public void Test2_1(){
-      assertEquals("", Recursion.changePi(""));
-    }
+  // Test when there are 7's and other digits mixed throughout the int, expects 3
+  @Test
+  public void Test1_6() {
+    assertEquals(3, Recursion.count7(7187371));
+  }
 
-    @Test
-    public void Test2_2(){
-      assertEquals("3.14", Recursion.changePi("pi"));
-    }
+  // Tests for changePi
 
-    @Test
-    public void Test2_3(){
-      assertEquals("3.14a3.14", Recursion.changePi("piapi"));
-    }
+  // Test when the string is empty, expects empty string
+  @Test
+  public void Test2_1() {
+    assertEquals("", Recursion.changePi(""));
+  }
 
-    @Test
-    public void Test2_4(){
-      assertEquals("3.143.143.14", Recursion.changePi("pipipi"));
-    }
+  // Test when the string is "pi", expects "3.14"
+  @Test
+  public void Test2_2() {
+    assertEquals("3.14", Recursion.changePi("pi"));
+  }
 
-    @Test
-    public void Test2_5(){
-      assertEquals("Pi3.14Pi", Recursion.changePi("PipiPi"));
-    }
+  // Test when the string is a mix of "pi" and other characters, expects "3.14a3.14"
+  @Test
+  public void Test2_3() {
+    assertEquals("3.14a3.14", Recursion.changePi("piapi"));
+  }
 
-    @Test
-    public void Test2_6(){
-      assertEquals("ILoveToEat3.14", Recursion.changePi("ILoveToEatpi"));
-    }
+  // Test when the string is a several "pi" in a row, expects "3.143.143.14"
+  @Test
+  public void Test2_4() {
+    assertEquals("3.143.143.14", Recursion.changePi("pipipi"));
+  }
 
-    @Test
-    public void Test3_1(){
-      int[] intArray = {0};
-      assertFalse(Recursion.array220(intArray, 0));
-    }
+  // Test when the string is a several "pi" in a row but with capitalization, expects "Pi3.14Pi"
+  @Test
+  public void Test2_5() {
+    assertEquals("Pi3.14Pi", Recursion.changePi("PipiPi"));
+  }
 
-    @Test
-    public void Test3_2(){
-      int[] intArray = {1, 10};
-      assertTrue(Recursion.array220(intArray, 0));
-    }
+   // Test when string contains "pi", numbers, and special characters, expects 25.3.14,89
+  @Test
+  public void Test2_6() {
+    assertEquals("25.3.14,89", Recursion.changePi("25.pi,89"));
+  }
 
-    @Test
-    public void Test3_3(){
-      int[] intArray = {1, 2, 3, 4, 5, 6, 60};
-      assertTrue(Recursion.array220(intArray, 0));
-    }
+  @Test
+  public void Test3_1() {
+    int[] intArray = { 0 };
+    assertFalse(Recursion.array220(intArray, 0));
+  }
 
-    @Test
-    public void Test3_4(){
-      int[] intArray = {1, 1, 1, 1, 1, 1};
-      assertFalse(Recursion.array220(intArray, 0));
-    }
+  @Test
+  public void Test3_2() {
+    int[] intArray = { 1, 10 };
+    assertTrue(Recursion.array220(intArray, 0));
+  }
 
-    @Test
-    public void Test3_5(){
-      int[] intArray = {1, 5, 2, 6, 1, 10, 54, 20, 20, 1};
-      assertTrue(Recursion.array220(intArray, 0));
-    }
+  @Test
+  public void Test3_3() {
+    int[] intArray = { 1, 2, 3, 4, 5, 6, 60 };
+    assertTrue(Recursion.array220(intArray, 0));
+  }
 
-    @Test
-    public void Test3_6(){
-      int[] intArray = {-1, -2, -3, -4, -5, -6, 0};
-      assertFalse(Recursion.array220(intArray, 0));
-    }
+  @Test
+  public void Test3_4() {
+    int[] intArray = { 1, 1, 1, 1, 1, 1 };
+    assertFalse(Recursion.array220(intArray, 0));
+  }
+
+  @Test
+  public void Test3_5() { 
+    int[] intArray = { 1, 5, 2, 6, 1, 10, 54, 20, 20, 1 };
+    assertTrue(Recursion.array220(intArray, 0));
+  }
+
+  @Test
+  public void Test3_6() {
+    int[] intArray = { -1, -2, -3, -4, -5, -6, 0 };
+    assertFalse(Recursion.array220(intArray, 0));
+  }
     
 }
