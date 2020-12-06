6c269209b1589a7e57e74ada9dce93c915da54b5
Jeffrey Lin <jeffrey.lin21@ycdsbk12.ca>
2020-12-06 03:56:56 +0000

    Refactored by simplifying base case

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -4,18 +4,17 @@ public class Recursion{
 
   public static int count7(int n){
     
-    if(n < 10){
-      if (n == 7){
-        return 1;
-      }
+    if (n == 0){
       return 0;
     }
 
     if (n % 10 == 7) {
       return 1 + count7(n / 10);
     }
+    else {
+      return count7(n / 10);
+    }
 
-    else return count7(n / 10);
   }
 
 }
