191ce010cf071f070b6676a38e11b38672b26cfc
Jeffrey Lin <jeffrey.lin21@ycdsbk12.ca>
2020-12-06 03:55:14 +0000

    Added base case and recursive step

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -3,10 +3,19 @@ package recursionAssignment;
 public class Recursion{
 
   public static int count7(int n){
-    if(n == 7){
-      return 1;
+    
+    if(n < 10){
+      if (n == 7){
+        return 1;
+      }
+      return 0;
     }
-    return 0;
+
+    if (n % 10 == 7) {
+      return 1 + count7(n / 10);
+    }
+
+    else return count7(n / 10);
   }
 
 }
--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -12,13 +12,16 @@ public class RecursionTest{
     @Test
     public void Test1(){
         assertEquals(0, Recursion.count7(0));
-
     }
 
     @Test
     public void Test2(){
         assertEquals(1, Recursion.count7(7));
+    }
 
+    @Test
+    public void Test3(){
+        assertEquals(3, Recursion.count7(777));
     }
 
     // add more tests
